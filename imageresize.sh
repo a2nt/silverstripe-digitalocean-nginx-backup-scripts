7d71e6d0320eec32f494e2e1e5b00bb11da25bab
Tony Air <tony@thewebdevelopmentagency.com>
2016-10-13 09:27:41 +0300

    Image resize improvement

--- imageresize.sh
+++ imageresize.sh
@@ -9,17 +9,17 @@ echo "Resizing images to 1900x1200"
 
 for line in $(find . -iname '*.png'); do
     echo "Resizing: $line"
-    mogrify -resize 1900x1200 "$line"
+    mogrify -resize 1900x1200 -depth 8 -filter Triangle -define filter:support=2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -strip "$line"
 done
 
 for line in $(find . -iname '*.jpg'); do
     echo "Resizing: $line"
-    mogrify -resize 1900x1200 "$line"
+    mogrify -resize 1900x1200 -depth 8 -filter Triangle -define filter:support=2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -strip "$line"
 done
 
 for line in $(find . -iname '*.jpeg'); do
     echo "Resizing: $line"
-    mogrify -resize 1900x1200 "$line"
+    mogrify -resize 1900x1200 -depth 8 -filter Triangle -define filter:support=2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -strip "$line"
 done
 
 find . -name "*.png~" -delete
